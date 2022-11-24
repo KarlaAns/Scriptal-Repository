41a234d3e86c110b088b2b211a9f287132cb518a
Karla Alejandra Ansatuña Andrade <kansatunauemee@gmail.com>
2022-11-23 21:03:46 -0500

    V1.0.6

--- 06-Code/StudentControlSystem/src/studentcontrolsystem/view/StudentControlSystem.java
+++ 06-Code/StudentControlSystem/src/studentcontrolsystem/view/StudentControlSystem.java
@@ -13,6 +13,7 @@ public class StudentControlSystem {
         int choiceTeachOrStud;
         int choiceTeacher;
         long searchId;
+        boolean exitFirstMenu=false;
         String searchPassword = "";
         teacher.getPassword();
         Scanner sc = new Scanner(System.in);
@@ -20,7 +21,6 @@ public class StudentControlSystem {
             // Primer menu, puede ser estudiante o profesor
             menuTeacherOrStudent();
             choiceTeachOrStud = sc.nextInt();
-            choiceVerifier(choiceTeachOrStud);
             switch (choiceTeachOrStud) {
                 case 1 -> {
 
@@ -30,24 +30,29 @@ public class StudentControlSystem {
                         System.out.println("Please try again: ");
                         searchId = sc.nextInt();
                     }
-                    System.out.println("Please enter your password");
+                    /*System.out.println("Please enter your password");
                     searchPassword = sc.next();
                     int areDifferent = searchPassword.compareTo(teacher.getPassword());
                     while (areDifferent!=0) {
                         System.out.println("Please try again: ");
                         searchPassword = sc.next();
                     }
-                    System.out.println("you made it yay");
+                    System.out.println("you made it yay");*/
                     do {
                         //Tercer menu para el profesor
-                        menuTeacher();
+                        printMenuTeacher();
                         choiceTeacher = sc.nextInt();
-                        choiceVerifier(choiceTeacher);
 
-                    }while(choiceTeacher !=4);
+                    } while (choiceTeacher != 3);
                 }
+                case 3 -> {
+                    exitFirstMenu = true;
+                }
+                default ->
+                    System.out.println("Invalid option");
+
             }
-        } while (choiceTeachOrStud != 3);
+        } while (!exitFirstMenu);
     }
 
     private static void menuTeacherOrStudent() {
@@ -58,23 +63,22 @@ public class StudentControlSystem {
         System.out.println("3.Exit the program > \t");
         System.out.println("=======================");
     }
-    private static void choiceVerifier(int choice) {
-        while (choice < 1 || choice > 3) {
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Invalid option, please enter again:  ");
-            choice = sc.nextInt();
-        }
-    }
-    private static void menuTeacher() {
+
+    private static void printMenuTeacher() {
         String name;
         name="Lucy";
         System.out.println("   Welcome " +name);
         System.out.println("=======================");
         System.out.println("1.Register a student > \t");
-        System.out.println("2.Enter into the grade system > \t");
-        System.out.println("3.Find Data about a student > \t");
-        System.out.println("4.Exit the teacher menu > \t");
+        //System.out.println("2.Enter into the grade system > \t");
+        System.out.println("2.Find Data about a student > \t");
+        System.out.println("3.Exit the teacher menu > \t");
         System.out.println("=======================");
+        menuTeacher();
+    }
+
+    private static void menuTeacher() {
+        
     }
 }
 
