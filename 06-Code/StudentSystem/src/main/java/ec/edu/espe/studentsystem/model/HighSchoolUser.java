5ed6319c3ce9e498034173ad376cee9991d49025
Alejo Andrade <02alejo20@gmail.com>
2023-01-12 20:30:09 -0500

    java with ant to java with maven

---
+++ 06-Code/StudentSystem/src/main/java/ec/edu/espe/studentsystem/model/HighSchoolUser.java
@@ -0,0 +1,10 @@
+
+package ec.edu.espe.studentsystem.model;
+
+/**
+ *
+ * @author Alejandro Andrade, Scriptal, DCCO_ESPE
+ */
+public abstract class HighSchoolUser {
+    
+}
---
+++ 06-Code/StudentSystem/src/main/java/ec/edu/espe/studentsystem/model/Student.java
@@ -0,0 +1,11 @@
+
+package ec.edu.espe.studentsystem.model;
+
+/**
+ *
+ * @author Alejandro Andrade, Scriptal, DCCO_ESPE
+ */
+public class Student extends HighSchoolUser{
+    
+    
+}
