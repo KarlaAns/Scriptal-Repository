0de7aa4d0029b7ebe1f09dae2b99f0d55ee66bae
Alejo Andrade <02alejo20@gmail.com>
2023-01-13 21:06:38 -0500

    Student System V4.1.7

--- 06-Code/StudentSystem/src/main/java/ec/edu/espe/studentsystem/view/FrmEnrollment.java
+++ 06-Code/StudentSystem/src/main/java/ec/edu/espe/studentsystem/view/FrmEnrollment.java
@@ -9,12 +9,12 @@ import javax.swing.UIManager;
  *
  * @author Alejandro Andrade, Scriptal, DCCO_ESPE
  */
-public class FrmStudent extends javax.swing.JFrame {
+public class FrmEnrollment extends javax.swing.JFrame {
 
     /**
-     * Creates new form FrmStudent
+     * Creates new form FrmEnrollment
      */
-    public FrmStudent() {
+    public FrmEnrollment() {
         initComponents();
     }
 
@@ -27,44 +27,17 @@ public class FrmStudent extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        lblStudent = new javax.swing.JLabel();
-        btnActivity = new javax.swing.JButton();
-        btnEnrollment = new javax.swing.JButton();
         jMenuBar1 = new javax.swing.JMenuBar();
         mnStudentSystem = new javax.swing.JMenu();
         mniAbout = new javax.swing.JMenuItem();
         mniLogOut = new javax.swing.JMenuItem();
         mnManage = new javax.swing.JMenu();
         mnActivity = new javax.swing.JMenuItem();
-        mnEnrollment = new javax.swing.JMenuItem();
         menuView = new javax.swing.JMenu();
         cbmiDarkMode = new javax.swing.JCheckBoxMenuItem();
         menuHelp = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 60)); // NOI18N
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("Welcome");
-
-        lblStudent.setFont(new java.awt.Font("Segoe UI", 0, 48)); // NOI18N
-        lblStudent.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblStudent.setPreferredSize(new java.awt.Dimension(0, 50));
-
-        btnActivity.setText("Activity");
-        btnActivity.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnActivityActionPerformed(evt);
-            }
-        });
-
-        btnEnrollment.setText("Enrollment");
-        btnEnrollment.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnEnrollmentActionPerformed(evt);
-            }
-        });
 
         mnStudentSystem.setText("StudentSystem");
 
@@ -91,14 +64,6 @@ public class FrmStudent extends javax.swing.JFrame {
         });
         mnManage.add(mnActivity);
 
-        mnEnrollment.setText("Enrollment");
-        mnEnrollment.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                mnEnrollmentActionPerformed(evt);
-            }
-        });
-        mnManage.add(mnEnrollment);
-
         jMenuBar1.add(mnManage);
 
         menuView.setText("View");
@@ -122,36 +87,22 @@ public class FrmStudent extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(lblStudent, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
-            .addGroup(layout.createSequentialGroup()
-                .addGap(351, 351, 351)
-                .addComponent(btnActivity)
-                .addGap(147, 147, 147)
-                .addComponent(btnEnrollment)
-                .addContainerGap(342, Short.MAX_VALUE))
+            .addGap(0, 1000, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(92, 92, 92)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
-                .addComponent(lblStudent, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(125, 125, 125)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnActivity)
-                    .addComponent(btnEnrollment))
-                .addContainerGap(150, Short.MAX_VALUE))
+            .addGap(0, 539, Short.MAX_VALUE)
         );
 
         pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void mniAboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniAboutActionPerformed
+        //frmAboutUs newMniAbout = new frmAboutUs();
+        //newMniAbout.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_mniAboutActionPerformed
 
     private void cbmiDarkModeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbmiDarkModeActionPerformed
         if (cbmiDarkMode.isSelected()) {
@@ -171,21 +122,7 @@ public class FrmStudent extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_cbmiDarkModeActionPerformed
 
-    private void btnActivityActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActivityActionPerformed
-        openFrmActivities();
-    }//GEN-LAST:event_btnActivityActionPerformed
-
-    private void mniAboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mniAboutActionPerformed
-        FrmAboutUs newMniAbout = new FrmAboutUs();
-        newMniAbout.setVisible(true);
-        this.dispose();
-    }//GEN-LAST:event_mniAboutActionPerformed
-
     private void mnActivityActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnActivityActionPerformed
-        openFrmActivities();
-    }//GEN-LAST:event_mnActivityActionPerformed
-
-    private void openFrmActivities() {
         FrmActivities frmActivities = new FrmActivities();
         frmActivities.setVisible(true);
         System.out.println(UIManager.getLookAndFeel().getName());
@@ -195,58 +132,33 @@ public class FrmStudent extends javax.swing.JFrame {
             frmActivities.setStatusCbmiDarkMode(true);
         }
         this.dispose();
-    }
-
-    private void mnEnrollmentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mnEnrollmentActionPerformed
-        openFrmEnrollment();
-    }//GEN-LAST:event_mnEnrollmentActionPerformed
-
-    private void btnEnrollmentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEnrollmentActionPerformed
-        openFrmEnrollment();
-    }//GEN-LAST:event_btnEnrollmentActionPerformed
-
-    private void openFrmEnrollment() {
-        FrmEnrollment frmEnrollment = new FrmEnrollment();
-        frmEnrollment.setVisible(true);
-        System.out.println(UIManager.getLookAndFeel().getName());
-        if("FlatLaf Light".equals(UIManager.getLookAndFeel().getName())){
-            frmEnrollment.setStatusCbmiDarkMode(false);
-        }else{
-            frmEnrollment.setStatusCbmiDarkMode(true);
-        }
-        this.dispose();
-    }
+    }//GEN-LAST:event_mnActivityActionPerformed
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
         Theme.setFlatLightLafTheme();
-
-        /* Create and display the form */
+        
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new FrmStudent().setVisible(true);
+                new FrmEnrollment().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnActivity;
-    private javax.swing.JButton btnEnrollment;
     private javax.swing.JCheckBoxMenuItem cbmiDarkMode;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JLabel lblStudent;
     private javax.swing.JMenu menuHelp;
     private javax.swing.JMenu menuView;
     private javax.swing.JMenuItem mnActivity;
-    private javax.swing.JMenuItem mnEnrollment;
     private javax.swing.JMenu mnManage;
     private javax.swing.JMenu mnStudentSystem;
     private javax.swing.JMenuItem mniAbout;
     private javax.swing.JMenuItem mniLogOut;
     // End of variables declaration//GEN-END:variables
+
     public boolean getStatusCbmiDarkMode() {
         return cbmiDarkMode.isSelected();
     }
@@ -254,4 +166,5 @@ public class FrmStudent extends javax.swing.JFrame {
     public void setStatusCbmiDarkMode(boolean isSelected) {
         this.cbmiDarkMode.setSelected(isSelected);
     }
+
 }
